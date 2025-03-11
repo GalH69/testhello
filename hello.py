8b917725258074b5d2aff5349b4b8c204a44456c
GalH69 <auto.install@cs.com>
2025-03-11 22:17:51 +0200

    'autosave'

--- hello.py
+++ hello.py
@@ -13,9 +13,10 @@ def dec_to_minusbin(num, amount):
     bin_num = int(text_bin_num, 2)
     
     
+    bin_num = bin(bin_num)[2:]
     bin_num_one = bin(1)[2:]
-    bin_num = bin_num + bin_num_one,2
+    bin_num = bin_num + bin_num_one
     
-    print(int(bin_num,2))
+    print(bin_num)
 dec_to_minusbin(5,8)
         
