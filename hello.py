dfb64b377501e544851fbd10b589f20a70ca4b23
GalH69 <auto.install@cs.com>
2025-03-11 22:13:35 +0200

    'autosave'

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ def dec_to_minusbin(num, amount):
     #to bin
     bin_num = bin(num)
     bin_num = bin_num[2:]
+    text_bin_num = 
     
     #complete to 2
     lst_bin = ['1' if bit == '0' else '0' for bit in bin_num]
