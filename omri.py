34767c9ce57d22ab94d1bb4c6a4b7f588f163d61
GalH69 <auto.install@cs.com>
2025-03-12 18:08:12 +0200

    'autosave'

--- omri.py
+++ omri.py
@@ -1,2 +1,6 @@
 def pita():
+    nitzav_1 = int(input("enter a number fo nitzav number 1"))
+    nitzav_2 = int(input("enter a number fo nitzav number 2"))
+    yeter = int(input("enter an number for yeter"))
     
+    pita = pow(nitzav_1,2) + 
