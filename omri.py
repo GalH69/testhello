3839992817c76945bb26658d2b0816ab9aace37b
GalH69 <auto.install@cs.com>
2025-03-12 18:11:08 +0200

    'autosave'

--- omri.py
+++ omri.py
@@ -3,8 +3,8 @@ import math
 
 
 def pita():
-    nitzav_1 = int(input("enter a number fo nitzav number 1":   ))
-    nitzav_2 = int(input("enter a number fo nitzav number 2":   ))
+    nitzav_1 = int(input("enter a number fo nitzav number 1:   "))
+    nitzav_2 = int(input("enter a number fo nitzav number 2:   "))
     
     pita = pow(nitzav_1,2) + pow(nitzav_2,2)
     yeter = math.sqrt(pita)
